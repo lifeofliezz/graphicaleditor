826aa96bc7ac954a5ac626e20900436d91b0663f
lt <lt@ivention.nl>
2023-06-21 14:08:06 +0200

    first commit graphical editor

---
+++ GraphicalEditor/MainForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace ShapeEditor
+{
+    public partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbEllipse = new System.Windows.Forms.RadioButton();
+            this.rbRectangle = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // rbEllipse
+            // 
+            this.rbEllipse.AutoSize = true;
+            this.rbEllipse.Location = new System.Drawing.Point(13, 13);
+            this.rbEllipse.Name = "rbEllipse";
+            this.rbEllipse.Size = new System.Drawing.Size(72, 17);
+            this.rbEllipse.TabIndex = 0;
+            this.rbEllipse.TabStop = true;
+            this.rbEllipse.Text = "draw elips";
+            this.rbEllipse.UseVisualStyleBackColor = true;
+            // 
+            // rbRectangle
+            // 
+            this.rbRectangle.AutoSize = true;
+            this.rbRectangle.Location = new System.Drawing.Point(13, 37);
+            this.rbRectangle.Name = "rbRectangle";
+            this.rbRectangle.Size = new System.Drawing.Size(95, 17);
+            this.rbRectangle.TabIndex = 1;
+            this.rbRectangle.TabStop = true;
+            this.rbRectangle.Text = "draw rectangle";
+            this.rbRectangle.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.rbRectangle);
+            this.Controls.Add(this.rbEllipse);
+            this.Name = "MainForm";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbEllipse;
+        private System.Windows.Forms.RadioButton rbRectangle;
+    }
+}
+
---
+++ GraphicalEditor/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ShapeEditor
+{
+    public partial class MainForm : Form
+    {
+        private List<Shape> shapes; // List to store all the shapes
+        private Shape selectedShape; // Currently selected shape
+        private Point lastMousePosition; // Last recorded mouse position
+
+        public MainForm()
+        {
+            InitializeComponent();
+            shapes = new List<Shape>();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            // Paint all the shapes
+            foreach (Shape shape in shapes)
+            {
+                shape.Draw(e.Graphics);
+            }
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Check if a shape is clicked
+                foreach (Shape shape in shapes)
+                {
+                    if (shape.Contains(e.Location))
+                    {
+                        if (selectedShape != null)
+                            selectedShape.IsSelected = false; // Deselect the previously selected shape
+
+                        selectedShape = shape;
+                        selectedShape.IsSelected = true; // Select the clicked shape
+                        lastMousePosition = e.Location;
+                        Invalidate();
+                        return;
+                    }
+                }
+
+                // If no shape is clicked, create a new shape
+                if (rbEllipse.Checked)
+                {
+                    shapes.Add(new Ellipse(e.Location));
+                }
+                else if (rbRectangle.Checked)
+                {
+                    shapes.Add(new RectangleShape(e.Location));
+                }
+
+                Invalidate();
+            }
+        }
+
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && selectedShape != null)
+            {
+                // Move the selected shape
+                int deltaX = e.Location.X - lastMousePosition.X;
+                int deltaY = e.Location.Y - lastMousePosition.Y;
+                selectedShape.Move(deltaX, deltaY);
+                lastMousePosition = e.Location;
+                Invalidate();
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            selectedShape = null;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (selectedShape != null)
+            {
+                int sizeChange = 5; // Amount of size change for each key press
+                switch (e.KeyCode)
+                {
+                    case Keys.Left:
+                        selectedShape.Resize(-sizeChange, 0);
+                        break;
+                    case Keys.Right:
+                        selectedShape.Resize(sizeChange, 0);
+                        break;
+                    case Keys.Up:
+                        selectedShape.Resize(0, -sizeChange);
+                        break;
+                    case Keys.Down:
+                        selectedShape.Resize(0, sizeChange);
+                        break;
+                }
+                Invalidate();
+            }
+        }
+    }
+
+    // Base class for all shapes
+    public abstract class Shape
+    {
+        protected Rectangle bounds; // Bounds of the shape
+        public bool IsSelected { get; set; } // Indicates if the shape is selected
+
+        public Shape(Point startPoint)
+        {
+            bounds = new Rectangle(startPoint, new Size(0, 0));
+        }
+
+        public bool Contains(Point point)
+        {
+            return bounds.Contains(point);
+            IsSelected = false; // Initialize IsSelected to false
+        }
+
+        public void Move(int deltaX, int deltaY)
+        {
+            bounds.X += deltaX;
+            bounds.Y += deltaY;
+        }
+
+        public void Resize(int deltaX, int deltaY)
+        {
+            bounds.Width += deltaX;
+            bounds.Height += deltaY;
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+
+    // Ellipse shape
+    public class Ellipse : Shape
+    {
+        public Ellipse(Point startPoint) : base(startPoint)
+        {
+            bounds.Width = 100;
+            bounds.Height = 100;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawEllipse(Pens.Black, bounds);
+        }
+    }
+
+    // Rectangle shape
+    public class RectangleShape : Shape
+    {
+        public RectangleShape(Point startPoint) : base(startPoint) 
+        {
+            bounds.Width = 100;
+            bounds.Height = 100;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawRectangle(Pens.Black, bounds);
+        }
+    }
+}
